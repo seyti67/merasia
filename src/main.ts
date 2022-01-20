23cffab214e9f9250146ece89d9da521929f2955
seyti67 <74255469+seyti67@users.noreply.github.com>
2022-01-20 20:07:58 +0100

    port

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 	app.useStaticAssets(join(__dirname, '..', 'public'));
 	app.use(cookieParser());
 	app.enableCors();
-	await app.listen(80);
+	await app.listen(process.env.PORT);
 }
 bootstrap();
 
